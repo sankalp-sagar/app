2a2fb840a45741b3cd92e9549370ca1d68f4d4f6
Sankalp Sagar <sankalpsagar1999@gmail.com>
2021-04-30 01:53:39 +0530

    new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destrtoy]
+    
     def show
-        @article = Article.find(params[:id])
+    
     end
 
     def index
@@ -9,12 +11,45 @@ class ArticlesController < ApplicationController
     end
 
     def new
-
+        @article = Article.new
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        redirect_to article_path(@article)
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article was saved successfully!"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
+
+    def edit
+    
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
 end
